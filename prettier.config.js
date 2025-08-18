cebd559e8060bd7bed2f911cda4c116e5fe1a1bb
kei0528 <keisuketanaka97@gmail.com>
2025-08-18 23:35:51 +0200

    feat: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+};
